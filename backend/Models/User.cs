a8ba6b7ad51e676f77a01882534e18fcee530382
alisafa60 <alisafa6100@gmail.com>
2024-02-04 23:28:39 +0200

    backend <> Created user model and db context

---
+++ backend/Models/User.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+public class User{
+    
+      public int Id {get; set;}
+
+      [EmailAddress]
+      public required string Email {get; set;}
+
+      [MinLength(8)]
+      public required string Password {get; set;}
+
+      public required string FirstName {get; set;}
+
+      public required string LastName {get; set;}
+
+      public string? PhoneNumber {get; set;}
+      
+      public string? Address {get; set;}
+
+      public required string Gender {get; set;}
+
+       public User(){
+        Gender = "Female";
+    }
+}
