915f6b5ab9100040b50a9b841200c79dd600d167
alisafa60 <alisafa6100@gmail.com>
2024-02-06 21:13:01 +0200

    backend <> adjusting models

--- backend/Enitities/User.cs
+++ backend/Enitities/User.cs
@@ -2,27 +2,35 @@ using System.ComponentModel.DataAnnotations;
 
 public class User{
     
-      public int Id {get; set;}
+    public int Id {get; set;}
+    
+    [Required]
+    [EmailAddress]
+    public string Email {get; set;}
 
-      [EmailAddress]
-      public required string Email {get; set;}
+    [Required]
+    [MinLength(8)]
+    public string Password {get; set;}
 
-      [MinLength(8)]
-      public required string Password {get; set;}
+    [Required]
+    public required string FirstName {get; set;}
 
-      public required string FirstName {get; set;}
+    [Required]
+    public required string LastName {get; set;}
 
-      public required string LastName {get; set;}
+    [Required]
+    public required DateTime DateOfBirth { get; set; }
 
-      public required DateTime DateOfBirth { get; set; }
-
-      public string? PhoneNumber {get; set;}
+    public string? PhoneNumber {get; set;}
       
-      public string? Address {get; set;}
+    public string? Address {get; set;}
 
-      public required string Gender {get; set;}
+    [Required]
+    public string Gender {get; set;}
 
-       public User(){
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public User(){
         Gender = "Female";
     }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 }
--- backend/Models/UserRegisterationModel.cs
+++ backend/Models/UserRegisterationModel.cs
@@ -2,25 +2,33 @@ using System.ComponentModel.DataAnnotations;
 
 public class UserRegisterationModel
 {
+    [Required]
     [EmailAddress]
-    public required string Email { get; set; }
+    public string Email { get; set; }
 
+    [Required]
     [MinLength(8)]
-    public required string Password { get; set; }
+    public string Password { get; set; }
 
-    public required string FirstName { get; set; }
+    [Required]
+    public string FirstName { get; set; }
 
-    public required string LastName { get; set; }
+    [Required]
+    public string LastName { get; set; }
 
-    public required DateTime DateOfBirth { get; set; }
+    [Required]
+    public DateTime DateOfBirth { get; set; }
 
     public string? PhoneNumber { get; set; }
 
     public string? Address { get; set; }
 
+    [Required]
     public string Gender { get; set; }
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public UserRegisterationModel()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
         Gender = "Female";
     }
