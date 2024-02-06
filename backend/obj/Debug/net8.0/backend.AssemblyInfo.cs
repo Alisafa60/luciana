f41ae6c6962301d845a93a64bcd7ec6a1f66ecee
alisafa60 <alisafa6100@gmail.com>
2024-02-07 00:44:59 +0200

    backend <> project structure adjustments

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5831922e76694287b03058b5ef5640bff5846451")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dcd9987f84b39eea3039f86623243350a6913d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
