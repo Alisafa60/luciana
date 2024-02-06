8831bbeb7e0df024803f8f3a5611febd8cead0f8
alisafa60 <alisafa6100@gmail.com>
2024-02-06 18:10:09 +0200

    backend <> created user registeration model

---
+++ backend/Models/UserRegisterationModel.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+public class UserRegisterationModel
+{
+    [EmailAddress]
+    public required string Email { get; set; }
+
+    [MinLength(8)]
+    public required string Password { get; set; }
+
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Address { get; set; }
+
+    public string Gender { get; set; }
+
+    public UserRegisterationModel()
+    {
+        Gender = "Female";
+    }
+}
