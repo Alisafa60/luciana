f7d4973ac234c7c290d3e74798ba10b5f61beb58
alisafa60 <alisafa6100@gmail.com>
2024-05-09 01:00:14 +0300

    backend <> adjusted startup class for seeding and middleware

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -1,36 +1,45 @@
+using System;
+using backend.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using backend.Data;
 using Microsoft.Extensions.Logging;
 
-public class Startup
-{
-    public Startup(IConfiguration configuration){
+public class Startup {
+    public Startup(IConfiguration configuration) {
         Configuration = configuration;
     }
 
     public IConfiguration Configuration { get; }
 
-    public void ConfigureServices(IServiceCollection services){
-
+    public void ConfigureServices(IServiceCollection services) {
         services.AddControllers();
-        
+
         var connectionString = Configuration.GetConnectionString("DefaultConnection");
         Console.WriteLine($"Connection String: {connectionString}");
 
         services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(connectionString)
         );
-
     }
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger){
-        if (env.IsDevelopment()){
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger) {
+        // Create a scope to resolve scoped services
+        using (var scope = app.ApplicationServices.CreateScope()) {
+            var serviceProvider = scope.ServiceProvider;
+
+            // Retrieve the necessary services
+            var dbContext = serviceProvider.GetRequiredService<AppDbContext>();
+
+            DataSeeder.SeedAdmin(dbContext);
+        }
+
+        if (env.IsDevelopment()) {
             app.UseDeveloperExceptionPage();
-        }else{
+        } else {
             app.UseExceptionHandler("/Home/Error");
             app.UseHsts();
         }
@@ -39,9 +48,12 @@ public class Startup
 
         app.UseRouting();
 
-        app.UseCors("AllowAllOrigins"); 
+        app.UseCors("AllowAllOrigins");
 
-        app.UseEndpoints(endpoints =>{
+        app.UseMiddleware<AuthMiddleware>();
+        app.UseMiddleware<AdminMiddleware>();
+
+        app.UseEndpoints(endpoints => {
             endpoints.MapControllers();
         });
     }
