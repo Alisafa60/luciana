f45ba7d6f333b64f0182ff9e15e02b88ff2bb742
alisafa60 <alisafa6100@gmail.com>
2024-05-23 17:39:23 +0300

    backend <> added parent fabric controller w/ post and get

---
+++ backend/Controllers/ParentFabricController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using backend.Data;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+
+[Route("api/parentfabric")]
+[ApiController]
+
+public class ParentFabricController : ControllerBase {
+    private readonly AppDbContext _context;
+    
+    public ParentFabricController(AppDbContext context) {
+        _context = context;
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public ActionResult<ParentFabricModel> CreateParentFabric(ParentFabricModel parentFabricModel) {
+        var parentFabric = new ParentFabric{
+            Name = parentFabricModel.Name,
+        };
+
+        _context.ParentFabrics.Add(parentFabric);
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(GetParentFabric), new {id = parentFabric.Id}, parentFabricModel);
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<ParentFabricModel>> GetParentFabrics() {
+        var parentFabrics = _context.ParentFabrics
+            .Select ( pf => new ParentFabricModel {Name = pf.Name,}).ToList();
+
+            return parentFabrics;
+    }
+
+    [HttpGet("{id}")]
+
+    public ActionResult<ParentFabricModel> GetParentFabric(int id) {
+        var parentFabric = _context.ParentFabrics.Find(id);
+        if (parentFabric == null) {
+            return NotFound();
+        }
+
+        var parentFabricModel = new ParentFabricModel{
+            Id = parentFabric.Id,
+            Name = parentFabric.Name,
+        };
+
+        return parentFabricModel;
+    }
+}
+
