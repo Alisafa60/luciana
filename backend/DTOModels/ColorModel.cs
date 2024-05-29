375b7d74b737904ae0b7844f8f11374046f2827e
alisafa60 <alisafa6100@gmail.com>
2024-05-29 21:37:54 +0300

    added color and parent color dto models

---
+++ backend/DTOModels/ColorModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+public class ColorModel {
+    public int Id { get; set; }
+
+    [Required]
+    public string HexValue { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public int ParentColorId { get; set; }
+}
