caff0e09df7ea0acb29f44bdf220fff5ae2ac3d4
alisafa60 <alisafa6100@gmail.com>
2024-06-02 20:21:53 +0300

    backend <> added product dto model

---
+++ backend/DTOModels/ProductModel.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+public class ProductModel {
+    public int Id { get; set; }
+    [Required]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    public string Name { get; set; }
+
+    public string? Description { get; set; }
+
+    [Required]
+    public decimal Price { get; set; }
+
+    public int? Stock { get; set; }
+
+    public bool ForChildren { get; set; }
+
+    public decimal Weight { get; set; }
+
+    public int ProductSizeId { get; set; }
+
+    public string ProductPicture { get; set; }
+
+    public ICollection<int> ProductTexturePatternIds { get; set; } = new List<int>();
+    public ICollection<int> ProductColorIds { get; set; } = new List<int>();
+    public ICollection<int> ProductFabricIds { get; set; } = new List<int>();
+    public ICollection<int> ProductCategoryIds { get; set; } = new List<int>();
+    public ICollection<int> ProductPromotionIds { get; set; } = new List<int>();
+}
