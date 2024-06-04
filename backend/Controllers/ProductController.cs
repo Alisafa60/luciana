658a73b44d941a2e4ecf8c7f08e1e056a8cf5cda
alisafa60 <alisafa6100@gmail.com>
2024-06-05 01:03:34 +0300

    backend <> replaced db context with repository in product table

--- backend/Controllers/ProductController.cs
+++ backend/Controllers/ProductController.cs
@@ -24,9 +24,7 @@ public class ProductController : ControllerBase {
         if (!ModelState.IsValid) {
             return BadRequest(ModelState);
         }
-
         try {
-
             string picturePath = null;
 
             if (productModel.Picture != null && productModel.Picture.Length > 0) {
@@ -63,29 +61,6 @@ public class ProductController : ControllerBase {
     public async Task<ActionResult<IEnumerable<ProductModel>>> GetProducts() {
         try {
             var products = await _productRepository.GetAllAsync();
-            // var products = await _context.Products
-            //     .Include(p => p.ProductTexturePatterns)
-            //     .Include(p => p.ProductColors)
-            //     .Include(p => p.ProductFabrics)
-            //     .Include(p => p.ProductCategories)
-            //     .Include(p => p.ProductPromotions)
-            //     .Select(p => new ProductModel {
-            //         Id = p.Id,
-            //         Name = p.Name,
-            //         Description = p.Description,
-            //         Price = p.Price,
-            //         Stock = p.Stock,
-            //         ForChildren = p.ForChildren,
-            //         Weight = p.Weight,
-            //         ProductSizeId = p.ProductSizeId,
-            //         ProductTexturePatternIds = p.ProductTexturePatterns.Select(ptp => ptp.TexturePatternId).ToList(),
-            //         ProductColorIds = p.ProductColors.Select(pc => pc.ColorId).ToList(),
-            //         ProductFabricIds = p.ProductFabrics.Select(pf => pf.FabricId).ToList(),
-            //         ProductCategoryIds = p.ProductCategories.Select(pc => pc.CategoryId).ToList(),
-            //         ProductPromotionIds = p.ProductPromotions.Select(pp => pp.PromotionId).ToList()
-            //     })
-            //     .ToListAsync();
-
             return Ok(products);
         } catch (Exception ex) {
             return StatusCode(500, $"Internal server error: {ex.Message}");
@@ -96,35 +71,12 @@ public class ProductController : ControllerBase {
     public async Task<ActionResult<ProductModel>> GetProduct(int id) {
         try {
             var product = await _productRepository.GetByIdAsync(id);
-            // var product = await _context.Products
-            //     .Include(p => p.ProductTexturePatterns)
-            //     .Include(p => p.ProductColors)
-            //     .Include(p => p.ProductFabrics)
-            //     .Include(p => p.ProductCategories)
-            //     .Include(p => p.ProductPromotions)
-            //     .FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) {
                 return NotFound();
             }
 
-            var productModel = new ProductModel {
-                Id = product.Id,
-                Name = product.Name,
-                Description = product.Description,
-                Price = product.Price,
-                Stock = product.Stock,
-                ForChildren = product.ForChildren,
-                Weight = product.Weight,
-                ProductSizeId = product.ProductSizeId,
-                ProductTexturePatternIds = product.ProductTexturePatterns.Select(ptp => ptp.TexturePatternId).ToList(),
-                ProductColorIds = product.ProductColors.Select(pc => pc.ColorId).ToList(),
-                ProductFabricIds = product.ProductFabrics.Select(pf => pf.FabricId).ToList(),
-                ProductCategoryIds = product.ProductCategories.Select(pc => pc.CategoryId).ToList(),
-                ProductPromotionIds = product.ProductPromotions.Select(pp => pp.PromotionId).ToList()
-            };
-
-            return Ok(productModel);
+            return Ok(product);
         } catch (Exception ex) {
             return StatusCode(500, $"Internal server error: {ex.Message}");
         }
@@ -143,62 +95,6 @@ public class ProductController : ControllerBase {
             return StatusCode(500, $"Internal Server Error {ex.Message}");
         }
     }
-
-    // [HttpPut("{id}")]
-    // [Authorize(Roles = "Admin")]
-    // public async Task<IActionResult> UpdateProduct(int id, ProductModel productModel) {
-    //     if (id != productModel.Id) {
-    //         return BadRequest();
-    //     }
-
-    //     var product = await _context.Products
-    //         .Include(p => p.ProductTexturePatterns)
-    //         .Include(p => p.ProductColors)
-    //         .Include(p => p.ProductFabrics)
-    //         .Include(p => p.ProductCategories)
-    //         .Include(p => p.ProductPromotions)
-    //         .FirstOrDefaultAsync(p => p.Id == id);
-
-    //     if (product == null) {
-    //         return NotFound();
-    //     }
-
-    //     try {
-    //         string picturePath = null;
-    //         if (productModel.Picture != null && productModel.Picture.Length > 0) {
-    //             picturePath = await SavePicture(productModel.Picture);
-    //         }
-
-    //         product.Name = productModel.Name;
-    //         product.Description = productModel.Description;
-    //         product.Price = productModel.Price;
-    //         product.Stock = productModel.Stock;
-    //         product.ForChildren = productModel.ForChildren;
-    //         product.Weight = productModel.Weight;
-    //         product.ProductSizeId = productModel.ProductSizeId;
-    //         product.ProductPicturePath = picturePath ?? product.ProductPicturePath;
-
-    //         product.ProductTexturePatterns = productModel.ProductTexturePatternIds.Select(tid => new ProductTexturePattern { ProductId = productModel.Id, TexturePatternId = tid }).ToList();
-    //         product.ProductColors = productModel.ProductColorIds.Select(cid => new ProductColor { ProductId = productModel.Id, ColorId = cid }).ToList();
-    //         product.ProductFabrics = productModel.ProductFabricIds.Select(fid => new ProductFabric { ProductId = productModel.Id, FabricId = fid }).ToList();
-    //         product.ProductCategories = productModel.ProductCategoryIds.Select(cid => new ProductCategory { ProductId = productModel.Id, CategoryId = cid }).ToList();
-    //         product.ProductPromotions = productModel.ProductPromotionIds.Select(pid => new ProductPromotion { ProductId = productModel.Id, PromotionId = pid }).ToList();
-
-    //         _context.Entry(product).State = EntityState.Modified;
-
-    //         await _context.SaveChangesAsync();
-
-    //         return NoContent();
-    //     } catch (DbUpdateConcurrencyException) {
-    //         if (!_context.Products.Any(p => p.Id == id)) {
-    //             return NotFound();
-    //         } else {
-    //             throw;
-    //         }
-    //     } catch (Exception ex) {
-    //         return StatusCode(500, $"Internal server error: {ex.Message}");
-    //     }
-    // }
 
     [HttpDelete("{id}")]
     [Authorize(Roles = "Admin")]
