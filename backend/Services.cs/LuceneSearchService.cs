34f4c9035c6a943499b365981ded771ff3037e72
alisafa60 <alisafa6100@gmail.com>
2024-06-06 16:28:30 +0300

    backend <> implemented Lucene search to search product and its attributes

--- backend/Services.cs/LuceneSearchService.cs
+++ backend/Services.cs/LuceneSearchService.cs
@@ -4,6 +4,7 @@ using Lucene.Net.Analysis.En;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Analysis.Util;
 using Lucene.Net.Documents;
+using Lucene.Net.Documents.Extensions;
 using Lucene.Net.Index;
 using Lucene.Net.QueryParsers.Classic;
 using Lucene.Net.Search;
@@ -11,6 +12,7 @@ using Lucene.Net.Store;
 using Lucene.Net.Util;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 
@@ -43,7 +45,68 @@ public class LuceneSearchService {
             var document = new Document {
                 new StringField("Id", product.Id.ToString(), Field.Store.YES),
                 new TextField("Name", product.Name, Field.Store.YES),
+                new TextField("Desctiption", product.Description ?? string.Empty, Field.Store.YES),
             };
+
+            foreach (var texturePatternId in product.ProductTexturePatternIds) {
+                document.Add(new Int32Field("TexturePatternId", texturePatternId, Field.Store.YES));
+            }
+
+            foreach (var colorId in product.ProductColorIds) {
+                document.Add(new Int32Field("ColorId", colorId, Field.Store.YES));
+            }
+
+            foreach(var fabricId in product.ProductFabricIds) {
+                document.Add(new Int32Field("FabricId", fabricId, Field.Store.YES));
+            }
+
+            foreach(var categoryId in product.ProductCategoryIds) {
+                document.Add(new Int32Field("CategoryId", categoryId, Field.Store.YES));
+            }
+
+            foreach(var tagId in product.ProductTagIds) {
+                document.Add(new Int32Field("TagId", tagId, Field.Store.YES));
+            }
+
+            foreach(var promotionId in product.ProductPromotionIds) {
+                document.Add(new Int32Field("PromotionId", promotionId, Field.Store.YES));
+            }
+
+            writer.UpdateDocument(new Term("Id", product.Id.ToString()), document);
+            writer.Commit();
+        }
+    }
+
+    public IEnumerable<ProductModel> SearchProducts(string searchTerm, bool fuzzySearch = false) {
+        using (var reader = DirectoryReader.Open(_indexDirectory)) {
+            var searcher = new IndexSearcher(reader);
+            Query query;
+
+            if (fuzzySearch){
+                query = new FuzzyQuery(new Term("Name", searchTerm.ToLower()), 2);
+            } else {
+                var parser = new MultiFieldQueryParser(LuceneVersion.LUCENE_48, new[] { "Name", "Description", "TexturePatternId",
+                    "FabricId", "ColorId", "TagId", "CategoryId", "PromotionId"}, _analyzer);
+                query = parser.Parse(searchTerm);
+            }
+
+            var hits = searcher.Search(query, 10).ScoreDocs;
+
+            foreach (var hit in hits) {
+                var doc = searcher.Doc(hit.Doc);
+                yield return new ProductModel {
+                    Id = int.Parse(doc.Get("Id")),
+                    Name = doc.Get("Name"),
+                    Description = doc.Get("Description"),
+                    ProductTexturePatternIds = doc.GetFields("TexturePatternId").Select(f => int.Parse(f.GetStringValue())).ToList(),
+                    ProductColorIds = doc.GetFields("ColorId").Select(f => int.Parse(f.GetStringValue())).ToList(),
+                    ProductFabricIds = doc.GetFields("FabricId").Select(f => int.Parse(f.GetStringValue())).ToList(),
+                    ProductPromotionIds = doc.GetFields("PromotionId").Select(f => int.Parse(f.GetStringValue())).ToList(),
+                    ProductCategoryIds = doc.GetFields("CatgoryId").Select(f => int.Parse(f.GetStringValue())).ToList(),
+                    ProductTagIds = doc.GetFields("TagId").Select(f => int.Parse(f.GetStringValue())).ToList(),
+                };
+            }
+
         }
     }
 }
